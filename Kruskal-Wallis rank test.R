35b0bf0344a1fb484d6e4e68abaa60658c0e4fb6
Xu Liu <114212622+joelu12138@users.noreply.github.com>
2025-06-26 08:33:41 +0800

    Update Kruskal-Wallis rank test.R

--- Kruskal-Wallis rank test.R
+++ Kruskal-Wallis rank test.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(ggpubr)
 library(rstatix)
 library(PMCMRplus)
-data<-read.csv("data.csv",header = T,row.names = 1)#data of ��-diversity or Relative abundance of gut microbial phyla
+data<-read.csv("data.csv",header = T,row.names = 1)#data of ¦Á-diversity or Relative abundance of gut microbial phyla
 head(data)
 res.kruskal <- data %>% kruskal_test(core ~ Group)
 res.kruskal
@@ -11,3 +11,13 @@ pwc2 <- data %>%
   wilcox_test(core ~ Group, p.adjust.method = "bonferroni")
 pwc2
 write.csv(pwc2,"Chromadoridae.csv")
+#Leave-One-Out Kruskal-Wallis analysis
+# Leave-one-out分析
+results_loo <- sapply(1:nrow(data), function(i) {
+  data_loo <- data[-i, ]
+  test <- kruskal_test(data_loo, Metazoa ~ Group)
+  return(test$p)
+})
+
+plot(results_loo, type = "b", main = "Leave-One-Out Kruskal p-values",
+     xlab = "Removed Sample Index", ylab = "p-value")
